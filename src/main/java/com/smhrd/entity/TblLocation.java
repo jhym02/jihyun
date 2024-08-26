25114952f4fa0503992d4352712f2eda39e716e0
Enoch <lshllsee@naver.com>
2024-08-26 19:24:20 +0900

    Enoch 24.08.26 3st commit

--- src/main/java/com/smhrd/entity/TblLocation.java
+++ src/main/java/com/smhrd/entity/TblLocation.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Entity // 해당 클래스가 Scan이 되어지고, Entity역할로써 기능하기 위한 어노테이션
